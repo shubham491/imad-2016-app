83221f8fecbbc72369feec23de09327673e135ce
Shubham Das <96bubun19@gmail.com>
2016-09-18 00:47:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.get('/article-one',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-three',function(req,res){
-   res.send(createTemplate(data));
+   res.send(createTemp(data));
 });
 app.get('/article-two',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
