ae6bb1118ba7e91b34bf0c66d85d1fb308c36ee0
Shubham Das <96bubun19@gmail.com>
2016-09-22 21:17:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,13 @@ alert('Hi,I am javascript');
 var blo=document.getElementById("main-blo");
 blo.innerHTML="Nice One";
 var imag=document.getElementById("imge");
+var marginleft=0;
+function moveRight(){
+   marginleft+=10;
+   imag.style.marginLeft=marginleft+"px"; 
+}
+}
 imag.onclick=function(){
- imag.style.marginLeft="50px";   
+    var interval=setInterval(moveRight,100);
+   
 };
