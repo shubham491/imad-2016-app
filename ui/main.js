4ab22608752220f32e759aad1ce2cc7f4c7a6ebf
Shubham Das <96bubun19@gmail.com>
2016-09-22 20:59:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 alert('Hi,I am javascript');
 var blo=document.getElementById("main-blo");
 blo.innerHTML="Nice One";
+var imag=document.getElementById("image");
+imag.onclick=function(){
+ imag.style.marginLeft="50px";   
+};
