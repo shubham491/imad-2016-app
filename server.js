9db06c6426dc2d48099fd04d37133ee56ecf2e53
Shubham Das <96bubun19@gmail.com>
2016-09-25 12:49:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,6 +72,9 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+app.get('/ui/main2.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main2.js'));
+});
 app.get('/article-one',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
