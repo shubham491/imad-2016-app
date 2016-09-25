0ac74c5b5329112712709eba5bbf09ce99619dd5
Shubham Das <96bubun19@gmail.com>
2016-09-25 11:42:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,13 +74,6 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/article-one',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-app.get('/name_submit/',function(req,res){
-   var name=req.query.name;
-   names.push(name);
-   //JSON:JavaScript Object Notation
-   
-   res.send(JSON.stringify(names));
-});
 var comments=[];
 app.get('/comm1_submit/',function(req,res){
    var name=req.query.name;
@@ -89,6 +82,14 @@ app.get('/comm1_submit/',function(req,res){
    
    res.send(JSON.stringify(comments));
 });
+app.get('/name_submit/',function(req,res){
+   var name=req.query.name;
+   names.push(name);
+   //JSON:JavaScript Object Notation
+   
+   res.send(JSON.stringify(names));
+});
+
 
   // res.send(JSON.stringify(names));
 app.get('/:daname',function(req,res){
