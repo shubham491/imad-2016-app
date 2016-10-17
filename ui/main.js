7be9d2bd9e685215227cfb358d40458a6200a74b
Shubham Das <96bubun19@gmail.com>
2016-10-17 19:54:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,137 +1,76 @@
-function responsiveMobileMenu() {
-	$('.nav').each(function() {
+console.log('Loaded!');
+var blo=document.getElementById("main-blo");
+blo.innerHTML="Nice One";
+alert('Hi,I am javascript');
 
-
-
-		// $(this).children('ul').addClass('nav-menu'); // mark main menu list
-
-
-		var $style = $(this).attr('nav-menu-style'); // get menu style
-		if (typeof $style == 'undefined' || $style == false) {
-			$(this).addClass('yoga'); // set yoga style if style is not defined
-		} else {
-			$(this).addClass($style);
-		}
-
-
-		/* 	width of menu list (non-toggled) */
-
-		var $width = 0;
-		$(this).find('ul li').each(function() {
-			$width += $(this).outerWidth();
-		});
-
-		// if modern browser
-
-		if ($.support.leadingWhitespace) {
-			// $(this).css('max-width', $width * 1.2 + 'px');
-		}
-		// 
-		else {
-			$(this).css('width', $width * 1.2 + 'px');
-		}
-
-	});
+var imag=document.getElementById("imge");
+var marginleft=0;
+function moveRight(){
+   marginleft+=1;
+   imag.style.marginLeft=marginleft+"px"; 
 }
 
-function getMobileMenu() {
-
-	/* 	build toggled dropdown menu list */
-
-	$('.nav').each(function() {
-		var menutitle = $(this).attr("nav-menu-title");
-		if (menutitle == "") {
-			menutitle = "";
-		} else if (menutitle == undefined) {
-			menutitle = "";
-		}
-		var $menulist = $(this).children('.nav-menu').html();
-		var $menucontrols = "<div class='nav-toggled-controls'><div class='nav-toggled-title'>" + menutitle + "</div><div class='nav-button'><span>&nbsp;</span><span>&nbsp;</span><span>&nbsp;</span></div></div>";
-		$(this).prepend("<div class='nav-toggled nav-closed'>" + $menucontrols + "<ul>" + $menulist + "</ul></div>");
-
-	});
-}
-
-function adaptMenu() {
-
-	/* 	toggle menu on resize */
-
-	$('.nav').each(function() {
-		// var $width = $(this).css('max-width');
-		// $width = $width.replace('px', ''); 
-		var $width = 768;
-		if ($(this).parent().width() < $width * 1.05) {
-			$(this).children('.nav-menu').hide(0);
-			$(this).children('.nav-toggled').show(0);
-		} else {
-			$(this).children('.nav-menu').show(0);
-			$(this).children('.nav-toggled').hide(0);
-		}
-	});
-
-}
-
-$(function() {
-
-	responsiveMobileMenu();
-	getMobileMenu();
-	adaptMenu();
-
-	/* slide down mobile menu on click */
-
-	$('.nav-toggled, .nav-toggled .nav-button').click(function() {
-		if ($(this).is(".nav-closed")) {
-			$(this).find('> ul').stop().show(300);
-			$(this).removeClass("nav-closed");
-		} else {
-			$(this).find(' > ul').stop().hide(300);
-			$(this).addClass("nav-closed");
-		}
-
-	});
-
-});
-/* 	hide mobile menu on resize */
-$(window).resize(function() {
-	adaptMenu();
-});
-
-$(document).ready(function() {
-	//test for touch events support and if not supported, attach .no-touch class to the HTML tag.
-	if (!("ontouchstart" in document.documentElement)) {
-		document.documentElement.className += " no-touch";
-	} else {
-		document.documentElement.className += " touch";
-	}
-
-
-	var $submenus = $('.nav.yoga .nav-menu li > ul');
-	$submenus.hide();
-	if ($('html').hasClass('no-touch')) {
-		$submenus.parent().hover(function() {
-			$(this).find('> ul').slideToggle('fast');
-		});
-	} else {
-		$submenus.parent().click(function(e) {
-			
-			$(this).find('> ul').slideToggle('fast');
-			e.preventDefault();
-			e.stopPropagation();
-		});
-	}
-
-	$submenus.parent().addClass('has-children');
-
-
-	var $submenusSm = $('.nav.yoga .nav-toggled li > ul');
-	$submenusSm.hide();
-	$submenusSm.parent().prepend('<div class="clicker"></div>');
-	$submenusSm.parent().children('a').addClass('toggle-sm');
-	var clicker = $('.clicker');
-	clicker.on('click', function(e){
-		e.stopPropagation();
-		$(this).parent().find('> ul').slideToggle();
-		$(this).parent().children('a').toggleClass('open');
-	});
-	
-});
+imag.onclick=function(){
+    var interval=setInterval(moveRight,50);
+   
+};
+//counter code
+var button=document.getElementById("counter");
+//var counter=0;
+button.onclick=function(){
+  //create a request
+  var request=new XMLHttpRequest();
+  //capture the response and store it in a variable
+  request.onreadystatechange=function(){
+    if(request.readyState===XMLHttpRequest.DONE)
+    {
+        if(request.status===200)
+        {
+            var counter=request.responseText;
+            var span=document.getElementById('coun');
+            span.innerHTML=counter.toString();
+        }
+    }
+  };
+  //render the variable in the correct span
+  /*counter+=1;
+  var span=document.getElementById("coun");
+  span.innerHTML=counter.toString();*/
+  //make a request
+  request.open('GET','http://shubham491.imad.hasura-app.io/counter');
+  request.send('null');
+  
+};
+//capture name and submit
+//var name1=document.getElementById("name");
+//var name=name1.value;
+var subm=document.getElementById("submit_btn");
+subm.onclick=function(){
+  //make a request to the server and send the name
+  //capture a list of names and render it as a list
+  //create a request
+  var request=new XMLHttpRequest();
+  //capture the response and store it in a variable
+  request.onreadystatechange=function(){
+    if(request.readyState===XMLHttpRequest.DONE)
+    {
+        if(request.status===200)
+        {
+  var names=request.responseText;
+  names=JSON.parse(names);
+  var list='';
+  for(var i=0;i<names.length;i++)
+  {
+      list+="<li>"+names[i]+"</li>";
+      
+  }
+  var ul=document.getElementById("name_list");
+  ul.innerHTML=list;
+        }
+    }
+};
+var name1=document.getElementById("name");
+var name=name1.value;
+request.open('GET','http://shubham491.imad.hasura-app.io/name_submit?name='+name);
+  request.send('null');
+};
