8aa8dad4229ec2a11d9c1d73c2890c6f989abd90
Shubham Das <96bubun19@gmail.com>
2016-10-17 19:22:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,19 +78,19 @@ app.get('/ui/styl.css', function (req, res) {
 });
 
 app.get('css/jquery.bxslider.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui/css', 'jquery.bxslider.css'));
+  res.sendFile(path.join(__dirname, 'css', 'css/jquery.bxslider.css'));
 });
 app.get('css/nav.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui/css', 'nav.css'));
+  res.sendFile(path.join(__dirname, 'css', 'nav.css'));
 });
 app.get('js/jquery-1.11.2.min.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui/js', 'jquery-1.11.2.min.js'));
+  res.sendFile(path.join(__dirname, 'js', 'jquery-1.11.2.min.js'));
 });
 app.get('js/jquery.bxslider.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui/js', 'jquery.bxslider.js'));
+  res.sendFile(path.join(__dirname, 'js', 'jquery.bxslider.js'));
 });
 app.get('js/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui/js', 'main.js'));
+  res.sendFile(path.join(__dirname, 'js', 'main.js'));
 });
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
