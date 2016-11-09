0864f9a78158e1eb4fb5ca98c57d07466b1c574e
Shubham Das <96bubun19@gmail.com>
2016-11-10 00:34:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,10 +85,10 @@ app.get('/', function (req, res) {
 app.get('/profile1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index1.html'));
 });
-app.post('/create-user',function(req,res){
+app.get('/create-user/:username/:password',function(req,res){
     var pool=new Pool(config);
-    var username=req.body.username;
-    var password=req.body.password;
+    var username=req.params.username;
+    var password=req.params.password;
     var salt=crypto.randomBytes(128).toString('hex');
     var dbString=hash(password,salt);
     pool.query('INSRT INTO "user" (username,password) VALUES($1,$2)',[username,dbString],function(err,result){
