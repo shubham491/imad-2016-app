61e7a3aafffd97551545aa23b8d4ac21260915e5
Shubham Das <96bubun19@gmail.com>
2016-11-10 10:46:20 +0530

    [imad-console] Updates ui/signup1.js

--- ui/signup1.js
+++ ui/signup1.js
@@ -1,5 +1,26 @@
  var submit = document.getElementById('submit_btn');
     submit.onclick = function () {
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+              loadLogin();
+          }  
+          // Not done yet
+        };
  var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
         console.log(username);
