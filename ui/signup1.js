62f057acdc3c92d25e48e247bf599b2170bc1799
Shubham Das <96bubun19@gmail.com>
2016-11-10 10:19:56 +0530

    [imad-console] Updates ui/signup1.js

--- ui/signup1.js
+++ ui/signup1.js
@@ -1,1 +1,10 @@
-
+ var submit = document.getElementById('submit_btn');
+    submit.onclick = function () {
+ var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+};
