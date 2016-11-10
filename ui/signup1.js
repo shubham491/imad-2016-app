a9d24b6b498832b3925cc6533d3744e5e6bc1224
Shubham Das <96bubun19@gmail.com>
2016-11-10 10:26:48 +0530

    [imad-console] Updates ui/signup1.js

--- ui/signup1.js
+++ ui/signup1.js
@@ -4,7 +4,7 @@
         var password = document.getElementById('password').value;
         console.log(username);
         console.log(password);
-        request.open('POST', '/create-user', true);
+        request.open('POST', 'http://shubham491.imad.hasura-app.io/create-user', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
 };
