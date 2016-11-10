5747a6ca8f249ae2140f1c27c7d0978316c61cfd
Shubham Das <96bubun19@gmail.com>
2016-11-10 16:09:31 +0530

    [imad-console] Updates ui/main3.js

--- ui/main3.js
+++ ui/main3.js
@@ -1,14 +1,4 @@
-var wrap = $("#wrap");
 
-wrap.on("scroll", function(e) {
-    
-  if (this.scrollTop > 147) {
-    wrap.addClass("fix-search");
-  } else {
-    wrap.removeClass("fix-search");
-  }
-  
-});
      
 
 
