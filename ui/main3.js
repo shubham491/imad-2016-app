7e75f11be4dbe02be0a0afeb69196695d32cfa46
Shubham Das <96bubun19@gmail.com>
2016-11-10 16:43:59 +0530

    [imad-console] Updates ui/main3.js

--- ui/main3.js
+++ ui/main3.js
@@ -44,7 +44,7 @@
         var password = document.getElementById('password').value;
         console.log(username);
         console.log(password);
-        request.open('POST', '/login', true);
+        request.open('POST', 'http://http://shubham491.imad.hasura-app.io/login', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
         submit.value = 'Logging in...';
